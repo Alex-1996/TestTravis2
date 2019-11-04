ea45ccbb673e4faa1688b2c4f63121a892935584
Alex-1996 <56635049+Alex-1996@users.noreply.github.com>
2019-11-04 19:43:09 +0100

    "modified files 8"

--- src/hello/HelloWorld3.java
+++ src/hello/HelloWorld3.java
@@ -1,6 +1,8 @@
 package hello;
+import java.util.logging.Logger;
 public class HelloWorld3 {
+	static Logger logger = Logger.getLogger(HelloWorld3.class.getName());
 	public static void main(String[] args) {
-		System.out.println("Hello World!")//NOPMD; 
+		logger.info("Hello World!");
 	}
 }
